df30faeb30b2e373e68a0e2304a88348c4a982f1
Matheus <maahwiski@gmail.com>
2022-12-13 02:00:07 -0300

    corrigido a correção que corrigia a correção que não corrigiu nada. O problema estava no main.js que não estava lidando com a promise segundo o cypress

--- src/main.js
+++ src/main.js
@@ -1,20 +1,9 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
-import { fetchProductsList } from './helpers/fetchFunctions';
-import { createProductElement } from './helpers/shopFunctions';
+// import { fetchProductsList } from './helpers/fetchFunctions';
+// import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
-const listOfProducts = async () => {
-  const data = await fetchProductsList('computador');
-  data.forEach((product) => {
-    const { id, title, price, thumbnail } = product;
-    const object = { id, title, price, thumbnail };
-    const InfoOfProducts = createProductElement(object);
-    const productsDiv = document.getElementsByClassName('.products');
-    productsDiv.appendChild(InfoOfProducts);
-    return InfoOfProducts;
-  });
-};
-
-listOfProducts();
+// const listOfProducts = async () => {
+// };
