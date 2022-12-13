aa94b5b03fd37d10114ede5c0c2c9231cdeaeee0
Matheus <maahwiski@gmail.com>
2022-12-13 04:43:58 -0300

    requisitos 4, 5, 7

--- src/main.js
+++ src/main.js
@@ -5,13 +5,40 @@ import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
+const container = document.querySelector('.products');
+const makeAMessageOfLoading = document.createElement('p');
+const errorMessage = document.createElement('p');
+
+const loading = () => {
+  container.appendChild(makeAMessageOfLoading);
+  makeAMessageOfLoading.className = 'loading';
+  makeAMessageOfLoading.textContent = 'carregando';
+};
+
+const removeLoading = () => {
+  makeAMessageOfLoading.remove();
+};
+
+const makeAnErrorAlert = () => {
+  errorMessage.className = 'error';
+  errorMessage.textContent = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  container.appendChild(errorMessage);
+};
+
 const listOfProducts = async () => {
-  const itens = await fetchProductsList('computador');
-  const products = document.querySelector('.products');
-  itens.forEach((item) => {
-    const createListOfElements = createProductElement(item);
-    products.appendChild(createListOfElements);
-  });
+  loading();
+  try {
+    const itens = await fetchProductsList('computador');
+    removeLoading();
+    const products = document.querySelector('.products');
+    itens.forEach((item) => {
+      const createListOfElements = createProductElement(item);
+      products.appendChild(createListOfElements);
+    });
+  } catch (error) {
+    removeLoading();
+    makeAnErrorAlert();
+  }
 };
 
 window.onload = () => {
